ccbd5db55cba5e9e73fecdd22789f9489aeb7958
Daniel Plinsinga <info@toastdesign.nl>
2017-07-18 09:51:44 +0200

    Removed off canvas

--- footer.php
+++ footer.php
@@ -10,22 +10,16 @@
 
 ?>
 
-		</section>
-		<div class="footer-container" data-sticky-footer>
-			<footer class="footer">
-				<?php do_action( 'foundationpress_before_footer' ); ?>
-				<?php dynamic_sidebar( 'footer-widgets' ); ?>
-				<?php do_action( 'foundationpress_after_footer' ); ?>
-			</footer>
-		</div>
+	</section>
+	<div class="footer-container" data-sticky-footer>
+		<footer class="footer">
+			<?php do_action( 'foundationpress_before_footer' ); ?>
+			<?php dynamic_sidebar( 'footer-widgets' ); ?>
+			<?php do_action( 'foundationpress_after_footer' ); ?>
+		</footer>
+	</div>
 
-		<?php do_action( 'foundationpress_layout_end' ); ?>
-
-<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
-		</div><!-- Close off-canvas content -->
-	</div><!-- Close off-canvas wrapper -->
-<?php endif; ?>
-
+	<?php do_action( 'foundationpress_layout_end' ); ?>
 
 <?php wp_footer(); ?>
 <?php do_action( 'foundationpress_before_closing_body' ); ?>
--- header.php
+++ header.php
@@ -19,11 +19,6 @@
 	<body <?php body_class(); ?>>
 	<?php do_action( 'foundationpress_after_body' ); ?>
 
-	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
-	<div class="off-canvas-wrapper">
-		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
-	<?php endif; ?>
-
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 
 	<header class="site-header" role="banner">
